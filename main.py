56bb5cf047c3dcaad218a22478f8a0e5b9b525f0
Batoul <me20m001@technikum-wien.at>
2021-04-06 17:38:19 +0200

    I changed "bye" to "this is a test"

--- main.py
+++ main.py
@@ -14,6 +14,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-    print("bye")
+    print("this is a test")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
